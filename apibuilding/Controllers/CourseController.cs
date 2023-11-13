ebf5090bbd3ffe5e3e92ef8f7930bb0db5d43593
Ayokunle <therealayokunle@gmail.com>
2023-11-13 17:36:54 +0100

    first commit

---
+++ apibuilding/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using apibuilding.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apibuilding.Controllers
+{
+
+    [ApiController]
+    [Route("/api/Course")]
+    public class CourseController : ControllerBase
+    {
+        private CourseService _course_service;
+
+        public CourseController(CourseService _svc)
+        {
+            _course_service = _svc;
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_course_service.GetCourses());
+        }
+    }
+}
